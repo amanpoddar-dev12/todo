67261580ceedf0b405a1a87af1d78ba8902bcc76
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-10-05 02:29:30 +0530

    update styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -25,26 +25,26 @@ export default function Task() {
   } = useContext(TaskContext);
 
   return (
-    <div className=" text-white h-screen flex flex-col items-center ">
-      <h1 className="text-7xl text-center underline font-mono font-extralight md:font-thin tracking-wide text-indigo-100">
+    <div className="text-white min-h-screen flex flex-col items-center px-4 md:px-8 lg:px-16 xl:px-24">
+      <h1 className="text-2xl sm:text-3xl lg:text-4xl text-center underline font-mono font-extralight md:font-thin tracking-wide text-indigo-100 mt-4">
         TODO
       </h1>
-      <div className="mt-8 flex items-center space-x-4">
+      <div className="mt-8 flex flex-row sm:flex-row items-center space-y-4 space-x-4 sm:space-y-0 sm:space-x-4 w-full sm:w-auto">
         <input
           type="text"
           placeholder="Enter your work...."
-          className="px-12 py-3 rounded-md bg-slate-800 text-white focus:outline-none border border-slate-700"
+          className="w-auto sm:w-auto px-4 sm:px-6 py-3 rounded-md bg-slate-800 text-white focus:outline-none border border-slate-700"
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
           onKeyDown={handleKeyDown}
         />
         <button
-          className="bg-indigo-600 p-3 rounded-full text-white"
+          className="bg-indigo-600 p-3 rounded-full text-white hover:bg-indigo-700 transition"
           onClick={(e) => handleAddnewTask(e)}
         >
           <FaPlus />
         </button>
-        <button onClick={handleClear}>
+        <button onClick={handleClear} className="text-white">
           {onClear ? (
             <CiSaveUp2 className="text-4xl" />
           ) : (
@@ -52,11 +52,12 @@ export default function Task() {
           )}
         </button>
       </div>
-      <div className="mt-8 flex flex-col space-y-5">
+
+      <div className="mt-8 flex flex-col space-y-5 w-full sm:w-auto">
         {taskArray.map((task) => (
           <div
             key={task.id}
-            className="px-10 py-2 text-2xl rounded-md bg-slate-800 text-white focus:outline-none border border-slate-700 flex justify-between items-center"
+            className="px-4 sm:px-10  py-2 text-lg sm:text-xl rounded-md bg-slate-800 text-white focus:outline-none border border-slate-700 flex justify-between items-center"
           >
             {task.isEdited ? (
               <input
@@ -80,7 +81,7 @@ export default function Task() {
             <div className="flex space-x-4 ml-4">
               <button
                 onClick={() => handleDelete(task.id)}
-                className="focus:outline-none"
+                className="focus:outline-none text-white hover:text-red-500 transition"
               >
                 <FaDeleteLeft className="text-2xl" />
               </button>
@@ -95,12 +96,18 @@ export default function Task() {
                 )}
               </button>
               {task.isEdited ? (
-                <button onClick={() => handleSave(task.id)}>
-                  <CiSaveUp2 className="text-green-500" />
+                <button
+                  onClick={() => handleSave(task.id)}
+                  className="focus:outline-none text-green-500"
+                >
+                  <CiSaveUp2 className="text-2xl" />
                 </button>
               ) : (
-                <button onClick={() => handleEdit(task.id)}>
-                  <FaEdit />
+                <button
+                  onClick={() => handleEdit(task.id)}
+                  className="focus:outline-none text-white hover:text-blue-400 transition"
+                >
+                  <FaEdit className="text-2xl" />
                 </button>
               )}
             </div>
